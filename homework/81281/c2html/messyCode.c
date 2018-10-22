1c234cc6ef11159206dc974786a6c7c977e69da4
YanaRG <alexkolbeim@gmail.com>
2018-10-22 23:25:06 +0300

    Update messyCode.c

--- homework/81281/c2html/messyCode.c
+++ homework/81281/c2html/messyCode.c
@@ -40,110 +40,93 @@ struct point* biggest_point(size_t size,
     }
     return biggest;
 }
+
+int matrix[3][3] = {{0, 0, 0}, {0, 1, 0}, {0, 1, 1}};
+int arr[3] = {0, 0, 0};
+char matrix[3][3] = {{'0', '0', '0'}, {'0', '1', '0'}, {'0', '1', '1'}};
 /*Problem Secrect DAA
 /// lalalallala
 */
 //// mauuu
-
-typedef struct temp
-{
-    int a;
-
-} classTemp;
-
-void classTemp_GetData(classTemp* pThis)
-{
-    printf("Enter value of a : ");
-    scanf("%d", &(pThis->a));
-}
-
-classTemp T;
-
-struct point* biggest_point(size_t size,
-                            struct point* points,
-                            struct point* (*point_compare)(struct point* a,
-                                                           struct point* b))
-{
-    int i;
-    struct point* biggest = NULL;
-
-    for (i = 0; i < size; i++)
-    {
-        biggest = point_compare(biggest, points + i);
-    }
-    return biggest;
-}
-
+int a, b;
 void swap(int* i, int* j)
 {
 int t = *i;
-*i = *j;
-(*i)->go();
+*i = *j;(*i)->go();
 *j = t;
-}int partition(int l, int r)
+do
+{
+printf("mau\n");
+    } while (i > 1);
+}
+int partition(int l, int r)
 {
 int randIdx = l + rand() % (r - l);
-
 int pivot = a[randIdx];
 
 swap(&a[l], &a[randIdx]);
-int ind = l;for (int i = l + 1; i < r; i++)
-{if (a[i] <= pivot){ind++;swap(&a[i], &a[ind]);
-                   switch (allala)
+int ind = l;
+for (int i = l + 1; i < r; i++)
+{
+if (a[i] <= pivot)
+{
+ind++;
+swap(&a[i], &a[ind]);
+switch (allala)
     {
-        case 0:
-            o = 9;
-            break;
-        default:
-            break;
-    }}
-}
+                case 0:
+                    o = 9;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
-swap(&a[l], &a[ind]);
-return ind;
-}void quickSort(int l, int r)
-{
-if (l < r)
-{
-int idx = partition(l, r);
-quickSort(l, idx);
-quickSort(idx + 1, r);
+    swap(&a[l], &a[ind]);
+    return ind;
 }
+void quickSort(int l, int r)
+{
+    if (l < r)
+    {
+        int idx = partition(l, r);
+        quickSort(l, idx);
+        quickSort(idx + 1, r);
+    }
 }
 int binarySearch(int k)
 {
-    
-int middle, left = 0, right = N - 1;
-if (k < a[left])return (-1);if (k == a[left])
-return (left + 1);
-if (k > a[right])
-return (-1);
-
-while (right - left > 1)
-{
-middle = (left + right) / 2;
-if (k <= a[middle])
+    int middle, left = 0, right = N - 1;
+    if (k < a[left])
+        return (-1);
+    if (k == a[left])
+        return (left + 1);
+    if (k > a[right])
+        return (-1);    while (right - left > 1)
+    {
+        middle = (left + right) / 2;
+        if (k <= a[middle])
 right = middle;
-else
-left = middle;
-}
-return ((right <= N - 1 && a[right] == k) ? (right + 1) : (-1));
+        else
+            left = middle;
+    }
+    return ((right <= N - 1 && a[right] == k) ? (right + 1) : (-1));
 }
 
 int main()
-{srand(time(0));
-scanf("%d", &N);for (int i = 0; i < N; ++i)
 {
-scanf("%d ", &a[i]);
-}
-quickSort(0, N);
-
-int tmp = 0;
-scanf("%d", &tmp);
-while (tmp != 0)
-{
+    srand(time(0));
+    scanf("%d", &N);
+    for (int i = 0; i < N; ++i)
+    {
+        scanf("%d ", &a[i]);
+    }
+    quickSort(0, N);
+    int tmp = 0;    scanf("%d", &tmp);while (tmp != 0)
+    {
 printf("%d\n", binarySearch(tmp));
-scanf("%d", &tmp);
-}
-return 0;
+        scanf("%d", &tmp);
+    }
+    return 0;
 }
