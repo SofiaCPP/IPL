ba0b76b41b3eaa57d17ec83acd9f353e861b7892
YanaRG <alexkolbeim@gmail.com>
2018-10-22 14:58:03 +0300

    Update c2html.pl

--- homework/81281/c2html/c2html.pl
+++ homework/81281/c2html/c2html.pl
@@ -23,48 +23,103 @@ mainy(File):-
     flattenMine(Tokens, TokenStream),
     prettify(TokenStream, PrettyStream),
     open('page.html', write, WFile),
-    write(WFile,"<!doctype>
+     write(WFile,'<!doctype>
     <html>
-    <head>
-        <title>hello.c</title>
-        <style>
-           .keyword {
-               color: blue;
-           }
-           .number {
-               color: violet;
-           }
-           .identifier {
-               color: green;
-           }
-           .operator {
-               font-style: bold;
-               color: blue;
-           }
-           .comment {
-               color: lime;
-           }
-           .string {
-               color: orange;
-           }
-           .parenthese {
-               font-style: bold;
-               color: brown;
-           }
-           .quotation {
-               font-style: bold;
-               color: black;
-           }
-           .unknown {
-               font-style: bold;
-               color: red;
-           }
-           .function {
-               color: purple;
-           }
-       </style>
-    </head>
-    <body>
-        <pre class=\"code\">"),
+
+        <head>
+            <title>hello.c</title>
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+            <style>
+                .keyword {
+                    color: blue;
+                }
+
+                .number {
+                    color: violet;
+                }
+
+                .identifier {
+                    color: green;
+                }
+
+                .operator {
+                    font-style: bold;
+                    color: blue;
+                }
+
+                .comment {
+                    color: lime;
+                }
+
+                .string {
+                    color: orange;
+                }
+
+                .parenthese {
+                    font-style: bold;
+                    color: brown;
+                }
+
+                .quotation {
+                    font-style: bold;
+                    color: black;
+                }
+
+                .unknown {
+                    font-style: bold;
+                    color: red;
+                }
+
+                .function {
+                    color: purple;
+                }
+
+                .collapsible {
+                    background-color: PaleGreen;
+                    cursor: pointer;
+                    padding: 10px;
+                    width: 100%;
+                    border: none;
+                    text-align: left;
+                    outline: none;
+                    font-size: 15px;
+                }
+
+                .active,
+                .collapsible:hover {
+                    background-color: PaleGoldenRod;
+                }
+
+                .content {
+                    padding: 0 18px;
+                    display: none;
+                    overflow: hidden;
+                    background-color: #f1f1f1;
+                }
+            </style>
+        </head>
+
+        <body>
+            <pre class=\"code\">'),
     tohtmlfile(WFile, PrettyStream),
+    write(WFile, ' <script>
+                    var coll = document.getElementsByClassName(\"collapsible\");
+                    var i;
+
+                    for (i = 0; i < coll.length; i++) {
+                      coll[i].addEventListener(\"click\", function() {
+                        this.classList.toggle(\"active\");
+                        var content = this.nextElementSibling;
+                        if (content.style.display === \"block\") {
+                          content.style.display = \"none\";
+                        } else {
+                          content.style.display = \"block\";
+                        }
+                      });
+                    }
+                    </script>
+                        </pre>
+                    </body>
+
+                    </html>'),
     close(WFile).
