cb41fcbad36a6a5d0624013c50e43b284c548e88
YanaRG <alexkolbeim@gmail.com>
2018-10-22 16:08:06 +0300

    Can identify structures and collapse them too

--- homework/81281/c2html/essentials.pl
+++ homework/81281/c2html/essentials.pl
@@ -7,27 +7,49 @@ flattenElem([H|T], [[H|T]]):- \+ is_list(H), !.
 flattenElem([H|T], [H|T]):- is_list(H), H = [H1|_], \+ is_list(H1), !.
 flattenElem([H|T], [[H|T]]):- is_list(H), H = [H1|_], is_list(H1), !.
 
-identifyFunctions([], []):- !.
-identifyFunctions([H|T], [NewH|R]):-
+identifyFunctionsAndStructures([], []):- !.
+identifyFunctionsAndStructures([H|T], [NewH|R]):-
+    append(_, [[tstruct|_]|_], H),
+    T = [T1|T2],
+    T1 == [[tleftBrace, "{"], [execNL, "\n"]],
+    gatherBody(T2, [], PackedStructBody, Rest, 1),
+    append([H, T1], PackedStructBody, NewH), !,
+    identifyFunctionsAndStructures(Rest, R).
+
+identifyFunctionsAndStructures([H|T], [NewH|R]):-
     append(_, [[tfunction|_]|_], H),
     T = [T1|T2],
     T1 == [[tleftBrace, "{"], [execNL, "\n"]],
-    gatherFunctionBody(T2, [], PackedFunctionBody, Rest, 1),
+    gatherBody(T2, [], PackedFunctionBody, Rest, 1),
     append([H, T1], PackedFunctionBody, NewH), !,
-    identifyFunctions(Rest, R).
-identifyFunctions([H|T], [H|R]):- !,
-    identifyFunctions(T, R).
+    identifyFunctionsAndStructures(Rest, R).
+identifyFunctionsAndStructures([H|T], [H|R]):- !,
+    identifyFunctionsAndStructures(T, R).
 
-gatherFunctionBody(Rest, Res, Res, Rest, 0):- !.
-gatherFunctionBody([H|T], Buff, Res, Rest, N):-
-    N =\= 0, H == [[tleftBrace, "{"], [execNL, "\n"]], !,
+% gatherFunctionBody(Rest, Res, Res, Rest, 0):- !.
+% gatherFunctionBody([H|T], Buff, Res, Rest, N):-
+%     N =\= 0, H == [[tleftBrace, "{"], [execNL, "\n"]], !,
+%     append(Buff, [H], NewBuff), N1 is N + 1,
+%     gatherFunctionBody(T, NewBuff, Res, Rest, N1).
+% gatherFunctionBody([H|T], Buff, Res, Rest, N):-
+%     N =\= 0, H == [[trightBrace, "}"], [execNL, "\n"],[execNL, "\n"]], !,
+%     append(Buff, [H], NewBuff), N1 is N - 1, !,
+%     gatherFunctionBody(T, NewBuff, Res, Rest, N1).
+% gatherFunctionBody([H|T], Buff, Res, Rest, N):-
+%     N =\= 0,
+%     append(Buff, [H], NewBuff), !,
+%     gatherFunctionBody(T, NewBuff, Res, Rest, N).
+
+gatherBody(Rest, Res, Res, Rest, 0):- !.
+gatherBody([H|T], Buff, Res, Rest, N):-
+    N =\= 0, H = [[H11|_]|_], H11 == tleftBrace, !,
     append(Buff, [H], NewBuff), N1 is N + 1,
-    gatherFunctionBody(T, NewBuff, Res, Rest, N1).
-gatherFunctionBody([H|T], Buff, Res, Rest, N):-
-    N =\= 0, H == [[trightBrace, "}"], [execNL, "\n"],[execNL, "\n"]], !,
+    gatherBody(T, NewBuff, Res, Rest, N1).
+gatherBody([H|T], Buff, Res, Rest, N):-
+    N =\= 0, H = [[H11|_]|_], H11 == trightBrace, !,
     append(Buff, [H], NewBuff), N1 is N - 1, !,
-    gatherFunctionBody(T, NewBuff, Res, Rest, N1).
-gatherFunctionBody([H|T], Buff, Res, Rest, N):-
+    gatherBody(T, NewBuff, Res, Rest, N1).
+gatherBody([H|T], Buff, Res, Rest, N):-
     N =\= 0,
     append(Buff, [H], NewBuff), !,
-    gatherFunctionBody(T, NewBuff, Res, Rest, N).
+    gatherBody(T, NewBuff, Res, Rest, N).
