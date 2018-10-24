9e5f153af10e98ed033e3a7e3f256f4ed8afff31
YanaRG <alexkolbeim@gmail.com>
2018-10-24 10:17:13 +0300

    Added new tokens (forgotten operators) and fixed some old predicates

--- homework/81281/c2html/tokens.pl
+++ homework/81281/c2html/tokens.pl
@@ -34,7 +34,7 @@ token([twhile, "while" ]) --> "while", !.
 token([tinclude, "#include" ]) --> "#include", !.
 
 token([tcomment1, IA]) --> comment1(I), !,
-    { append(_, [_], I), atom_chars(IA,I)}.
+    { atom_chars(IA,I)}.
 
 %% Handling windows written files with "\r\n"
 token([[tcomment2, IA], [execNL, '\n']]) --> comment2(I), !,
@@ -53,7 +53,8 @@ token([[tfunction, IA], [tleftParen, "("]]) --> func(I), !,
     {append(IO, [_], I), IO \= [], atom_chars(IA, IO)}.
 token([tnumber, I]) --> number(I), !.
 
-
+token([tbinShiftLeftAssign, "<<="]) --> "<<=", !.
+token([tbinShiftRightAssign, ">>="]) --> ">>=", !.
 token([tpp, "++"]) --> "++", !.
 token([tmm, "--"]) --> "--", !.
 token([tptr, "->"]) --> "->", !.
@@ -61,8 +62,19 @@ token([tgreaterEqual,">=" ]) --> ">=", !.
 token([tequal, "=="]) --> "==", !.
 token([tnotequal,"!=" ]) --> "!=", !.
 token([tlessEqual, "<="]) --> "<=", !.
+token([tplusAssign, "+="]) --> "+=", !.
+token([tminusAssign, "-="]) --> "-=", !.
+token([tmultAssign, "*="]) --> "*=", !.
+token([tdivAssign, "/="]) --> "/=", !.
+token([tlessEqual, "<="]) --> "<=", !.
 token([tlogicalAnd, "&&"]) --> "&&", !.
 token([tlogicalOr, "||"]) --> "||", !.
+token([tbinShiftLeft, "<<"]) --> "<<", !.
+token([tbinShiftRight, ">>"]) --> ">>", !.
+token([tbinAndAssign, "&="]) --> "&=", !.
+token([tbinXOrAssign, "^="]) --> "^=", !.
+token([tbinOrAssign, "|="]) --> "|=", !.
+
 token([tmultiply, "*"]) --> "*", !.
 token([tdivide, "/"]) --> "/", !.
 token([tmod, "%"]) --> "%", !.
@@ -74,12 +86,17 @@ token([tnot, "!"]) --> "!", !.
 token([tassign,"="]) --> "=", !.
 token([tbitwiseAnd, "&"]) --> "&", !.
 token([tbitwiseOr, "|"]) --> "|", !.
+token([tbitwiseXOr, "^"]) --> "^", !.
+token([tbinCompliment, "~"]) --> "~", !.
 
-token([tspace , " "]) --> "\s", !.
+
+token([tspace , "\\s"]) --> "\\s", !.
 token([ttab , "\\t"]) --> "\\t", !.
+token([tnl, "\\n"]) --> "\\n", !.
+token([execSpace , "\s"]) --> "\s", !.
 token([execTAB , "\s\s\s\s"]) --> "\t", !.
 token([execNL, "\n"]) --> ( "\n"; "\r\n"), !.
-token([tnl, "\\n"]) --> "\\n", !.
+
 
 token([tleftParen,"("]) --> "(", !.
 token([trightParen, ")"]) --> ")", !.
@@ -160,12 +177,12 @@ allThatIsNotAToken([]) --> [].
 anythingButNL([C|Cs]) --> [C],
     {\+ char_code('\r', C), \+ char_code('\n', C)},
     anythingButNL(Cs).
-anythingButNL([C1,C2|_]) --> [C1,C2],
+anythingButNL([C1,C2]) --> [C1,C2],
     {(char_code('\r', C1),  char_code('\n', C2));
     (\+ char_code('\r', C1),  char_code('\n', C2))}.
 
 
-anythingButStarSlash([C1,C2|_]) --> [C1, C2],
+anythingButStarSlash([C1,C2]) --> [C1, C2],
     {char_code('*', C1),  char_code('/', C2)}.
 anythingButStarSlash([C|Cs]) --> [C],
     anythingButStarSlash(Cs).
@@ -175,12 +192,14 @@ anythingButStarSlash([C|Cs]) --> [C],
 %% Definitions of lists of different types of tokens
 is_keyWord([H,_]):- member(H, [tauto, tbreak, tcase, tchar, tconst, tcontinue,
     tdefault, tdo, tdouble, telse, tenum, textern, tfloat, tfor, tgoto, tif,
-    tint, tlong, tregister, treturn, tshort, tsigned, tsizeof, tstatic, tstruct,
+    tint, tlong, tregister, treturn, tshort, tsigned, tstatic, tstruct,
     tswitch, ttypedef, tunion, tunsigned, twhile, tvoid, tvolatile, tinclude]).
 
 is_operator([H,_]):- member(H, [tmultiply, tdivide, tmod, tadd, tsubtract, tless,
     tlessEqual, tgreater, tgreaterEqual, tequal, tnotequal, tnot, tassign, tlogicalOr,
-    tlogicalAnd, tbitwiseOr, tbitwiseAnd, tpp, tmm, tptr]).
+    tlogicalAnd, tbitwiseOr, tbitwiseAnd, tpp, tmm, tptr, tsizeof, tbinShiftLeftAssign,
+    tbinShiftRightAssign, tplusAssign, tminusAssign, tmultAssign, tdivAssign, tbinShiftLeft,
+    tbinShiftRight, tbinAndAssign, tbinXOrAssign, tbinOrAssign, tbitwiseXOr, tbinCompliment]).
 
 is_parenth([H,_]):- member(H, [tleftParen, trightParen, tleftBrace, trightBrace,
     tleftSqParen, trightSqParen, tsemicolon, ttwodots]).
@@ -197,9 +216,9 @@ is_controlStructureBody([H|_]):-
     member(Token, [tdo, tfor, twhile, tif, telse, tswitch, tfunction, tstruct]),
     append(_, [[Token|_]|_], H).
 
-is_identifier([H,_]):- member(H, [tidentifier, tnl, ttab]).
+is_identifier([H,_]):- member(H, [tidentifier, tnl, ttab, tspace]).
 
-is_blank([H,_]):- member(H, [execTAB, tspace, execNL]).
+is_blank([H,_]):- member(H, [execTAB, execSpace, execNL]).
 
 is_comment([H,_]):-member(H, [tcomment1, tcomment2]).
 
