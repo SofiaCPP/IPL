9bad961557504fa0c31cfb00e9e8baf37a45fc5b
Георги Бърнев <gbarnev@abv.bg>
2018-10-25 16:43:30 +0300

    Initial lexer structure

---
+++ homework/81709/javascript-syntax-highlighter/javascript-syntax-highlighter/NumToken.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace javascript_syntax_highlighter
+{
+    class NumToken : Token, IEquatable<NumToken>
+    {
+        public NumToken(int num) : base(TokenType.Number)
+        {
+            Num = num;
+        }
+
+        public int Num { get; private set; }
+
+        public bool Equals(NumToken other)
+        {
+            return other.Num == Num && base.Equals((Token)other);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as NumToken;
+
+            if (item == null) return false;
+
+            return Equals(item);
+        }
+
+        public override int GetHashCode()
+        {
+            return Num.GetHashCode() * 17 + base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Num.ToString();
+        }
+    }
+}
