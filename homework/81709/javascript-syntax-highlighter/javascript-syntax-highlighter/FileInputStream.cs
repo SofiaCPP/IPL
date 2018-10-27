5461cc5975617d09a6cef593d62380bc44478308
Георги Бърнев <gbarnev@abv.bg>
2018-10-27 14:32:19 +0300

    Extend InputStream interface

--- homework/81709/javascript-syntax-highlighter/javascript-syntax-highlighter/FileInputStream.cs
+++ homework/81709/javascript-syntax-highlighter/javascript-syntax-highlighter/FileInputStream.cs
@@ -10,10 +10,22 @@ namespace javascript_syntax_highlighter
     class FileInputStream : IInputStream
     {
         private StreamReader fileStr;
+        private int line;
+
+        public FileInputStream(string path)
+        {
+            fileStr = new StreamReader(path, Encoding.ASCII);
+
+        }
+
+        public bool IsEOF()
+        {
+            throw new NotImplementedException();
+        }
 
         public int Line()
         {
-            throw new NotImplementedException();
+            return this.line;
         }
 
         public char Next()
