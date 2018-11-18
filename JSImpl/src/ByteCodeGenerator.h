12232668d931b0f3593807d2096a2c2f26844b0b
Martin Dobrev <martindobrev0@gmail.com>
2018-11-18 20:32:58 +0200

    Code generation improvements (#56)
    * Implement code generation for some unary expressions
    Implement debug code augmentation.
    Now part of expresions have source location.
    Extend Tokens with Column information.
    Refactor byte code generation.
    Add options for byte code generator.
    
    * Fix linux build
    
    * Little refactoring in byte code generation
    
    * Fix couple bugs in byte code generation and add some tests.
    
    * Fix some linux warnings.

--- JSImpl/src/ByteCodeGenerator.h
+++ JSImpl/src/ByteCodeGenerator.h
@@ -2,5 +2,15 @@
 
 #include "Expression.h"
 
+struct ByteCodeGeneratorOptions
+{
+	enum OptimizationsType
+	{
+		None
+	};
+	ByteCodeGeneratorOptions(OptimizationsType o = None, bool debug = false) : Optimisations(o), AddDebugInformation(debug) {}
+	OptimizationsType Optimisations;
+	bool AddDebugInformation;
+};
 
-IPLString GenerateByteCode(ExpressionPtr program);
+IPLString GenerateByteCode(ExpressionPtr program, const IPLString& source, const ByteCodeGeneratorOptions& options = ByteCodeGeneratorOptions());
