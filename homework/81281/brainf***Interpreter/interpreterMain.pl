94ea551501a130b45a795c372d49a8aede276950
YanaRG <alexkolbeim@gmail.com>
2018-11-27 12:11:46 +0200

    HW3.2 Brainf interpreter (#43)
    * Create README.md
    
    * Initial commit
    
    * Initial commit
    
    * Create README.md
    
    * Rename homework/81281/c2html.pl to homework/81281/c2html/c2html.pl
    
    * Rename homework/81281/test.c to homework/81281/c2html/test.c
    
    * Update README.md
    
    * Update README.md
    
    * Update c2html.pl
    
    * Update c2html.pl
    
    * Update c2html.pl
    
    * Update README.md
    
    * Update c2html.pl
    
    * Update test.c
    
    * Create executeProgram.sh
    
    * Update README.md
    
    * Update c2html.pl
    
    * Initial commit of updated code + prettify C
    
    * Rename secret.c to messyCode.c
    
    * Update and rename test.c to initialTest.c
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Rename executeProgram.sh to c2htmlrun.sh
    
    * Update README.md
    
    * Update prettify.pl
    
    * Update prettify.pl
    
    * Update messyCode.c
    
    * Fixed some bugs.
    
    * Update README.md
    
    * Update c2html.pl
    
    * Added collapse and fixed matrix representation.
    
    * Updated flatten and added predicate for function identification.
    
    * Update initialTest.c
    
    * Update messyCode.c
    
    * Bug fixing, added is_functionAndBody predicate.
    
    * Update print.pl
    
    * Result after applying prettify and added collapse functions
    
    * Can identify structures and collapse them too
    
    * Can identify structures and collapse them too
    
    * Can identify structures and collapse them too
    
    * Can identify structures and collapse them too
    
    * Can identify structures and collapse them too
    
    * Update messyCode.c
    
    * Update README.md
    
    * Update prettify.pl
    
    * Update README.md
    
    * Update prettify.pl
    
    * Update messyCode.c
    
    * Update messyCode.c
    
    * Update page.html
    
    * Fix bug typedef
    
    * Update page.html
    
    * Added choices
    
    * Update c2html.pl
    
    * Update prettify.pl
    
    * Update prettify.pl
    
    * Update README.md
    
    * Update README.md
    
    * Update c2html.pl
    
    * Update c2html.pl
    
    * Added forgotten do-while structure format
    
    * Update messyCode.c
    
    * Removing unknown tokens from stream
    
    * Rename homework/81281/c2html/page.html to homework/81281/c2html/exampleCodeAndTestPage/page.html
    
    * Rename homework/81281/c2html/messyCode.c to homework/81281/c2html/exampleCodeAndTestPage/messyCode.c
    
    * Rename homework/81281/c2html/initialTest.c to homework/81281/c2html/exampleCodeAndTestPage/initialTest.c
    
    * Update prettify.pl
    
    * Can collapse all control structures and fixed comment /**/
    
    * Can collapse all control structures and fixed comment /**/
    
    * Can collapse all control structures and fixed comment /**/
    
    * Can collapse all control structures and fixed comment /**/
    
    * Can collapse all control structures and fixed comment /**/
    
    * Fixed named of predicates
    
    * Update messyCode.c
    
    * Update page.html
    
    * Update README.md
    
    * Changed execTAB to "\s\s\s\s"
    
    * Changed execTAB to "\s\s\s\s"
    
    * Changed execTAB to "\s\s\s\s"
    
    * Update page.html
    
    * Added new tokens (forgotten operators) and added rules for them
    
    * Added new tokens (forgotten operators) and fixed some old predicates
    
    * Fixed bug with "else '\n' if (..)" in addNewLines predicate
    
    * Fixed regular expression for a string and removed token ttypesPrintF
    
    * Added clauses for handling enum and fixed handling of chars
    
    * Fixed typos
    
    * Added forgotten token to clause
    
    * Fixed "case smt :'\n'"  where smt is more than one char
    
    * Fixed some bugs and divided code in modules
    
    * New code module and fixed bugs
    
    * Fixed some bugs with tabulation
    
    * Divided code in modules
    
    * Delete executeProgram.sh
    
    * Delete test.c
    
    * Initial version, still very buggy.
    
    * Finished with bug killing.
    
    * Fixed readme and added new program for tests.
    
    * Forgotten line added.
    
    * Organizing stuff in different folders.
    
    * Forgot to delete files.
    
    * Fixed typo.

---
+++ homework/81281/brainf***Interpreter/interpreterMain.pl
@@ -0,0 +1,35 @@
+:- use_module(library(dcg/basics)).
+:- use_module(library(readutil)).
+:- use_module(library(quintus)).
+:- [brainySource].
+
+:- set_prolog_flag(verbose, silent).
+:- initialization(main, main).
+
+main:-
+    current_prolog_flag(argv, Argv),
+    Argv = [H1|_],
+    atom_string(H1, Filename),
+    ((Filename == "", readyToReadAndInterpret("Hello, how can I help you?\nExit me by typing \"quit\"."));
+    (Filename \= "", readyToReadAndInterpret("Ready to interpret your file.\n", Filename))),
+    halt.
+main:-
+    halt(1).
+
+readyToReadAndInterpret(Message):-
+    write(Message),
+    startCycleTillQuit.
+
+readyToReadAndInterpret(Message, File):-
+    write(Message),
+    open(File, read, RFile),
+    read_stream_to_codes(RFile, BFSource),
+    close(RFile),
+    interpret(BFSource).
+
+startCycleTillQuit:-
+    nl,
+    current_stream(0, read, Stream),
+    read_line_to_codes(Stream, BFProgram),
+    ((string_codes("quit", BFProgram), write("Bye!"), nl);
+    (interpret(BFProgram), startCycleTillQuit)).
