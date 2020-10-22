d5fd7629fc6b85df0f27f3069bffef407a94cef8
martin dobrev <martindobrev0@gmail.com>
2020-10-22 23:18:55 +0300

    Reorganize ASTPrinter.

--- JSImpl/src/ASTPrinter.h
+++ JSImpl/src/ASTPrinter.h
@@ -2,42 +2,4 @@
 
 #include "ExpressionVisitor.h"
 
-#include <iosfwd>
-class ASTPrinter : public ExpressionVisitor
-{
-public:
-	ASTPrinter(std::ostream& os);
-	virtual ~ASTPrinter() {}
-	virtual void Visit(LiteralNull* e) override;
-	virtual void Visit(LiteralUndefined* e) override;
-	virtual void Visit(LiteralString* e) override;
-	virtual void Visit(LiteralNumber* e) override;
-	virtual void Visit(LiteralBoolean* e) override;
-	virtual void Visit(LiteralObject* e) override;
-	virtual void Visit(BinaryExpression* e) override;
-	virtual void Visit(UnaryExpression* e) override;
-	virtual void Visit(IdentifierExpression* e) override;
-	virtual void Visit(ListExpression* e) override;
-	virtual void Visit(VariableDefinitionExpression* e) override;
-	virtual void Visit(BlockStatement* e) override;
-	virtual void Visit(LabeledStatement* e) override;
-	virtual void Visit(IfStatement* e) override;
-	virtual void Visit(SwitchStatement* e) override;
-	virtual void Visit(CaseStatement *) override;
-	virtual void Visit(WhileStatement* e) override;
-	virtual void Visit(ForStatement* e) override;
-	virtual void Visit(FunctionDeclaration* e) override;
-	virtual void Visit(TopStatements* e) override;
-	virtual void Visit(EmptyExpression* e) override;
-	virtual void Visit(CallExpression* e) override;
-
-	inline void Enter();
-	inline void Exit();
-	inline void NoIndentNextAccept();
-	inline void IndentNextAccept();
-	inline void InsertIndent();
-private:
-	std::ostream& os;
-	unsigned nest_level;
-	bool indent_next_accept;
-};
+void PrintAST(const ExpressionPtr& ast, std::ostream& where);
