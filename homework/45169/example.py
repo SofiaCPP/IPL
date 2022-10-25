b95947ee3d624485a52e988b9b3e85c42d83de70
Stanislav Todorov <stodorov@vmware.com>
2022-10-25 21:18:42 +0300

    First homework: python2html

---
+++ homework/45169/example.py
@@ -0,0 +1,21 @@
+# This is an example program to test python lexer
+"""
+Multiline
+string
+example
+"""
+
+x = 5
+if x > 2:
+    print("X is greater than two!")
+else:
+    print("X is greater than two!")
+ 
+def myfunc():
+ global y
+ y = "World"
+ print("Hello " + y)
+
+myfunc()
+
+str("1")
